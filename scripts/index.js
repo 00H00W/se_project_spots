03534568dfdd0fd4030f56048c6445bf66cc0330
Sam Branham <hotdogsb@gmail.com>
2024-10-20 15:11:31 -0500

    Project 4 Stage 1

---
+++ scripts/index.js
@@ -0,0 +1,26 @@
+const initialCards = [
+  {
+    name: "Angkor Thom, Cambodia",
+    link: "https://unsplash.com/photos/a-long-hallway-with-stone-walls-and-pillars-HVDSaAoyou4",
+  },
+  {
+    name: "Petra, Jordan",
+    link: "https://unsplash.com/photos/brown-concrete-building-during-daytime-V_JINc1uYXY",
+  },
+  {
+    name: "Machu Picchu, Peru",
+    link: "https://unsplash.com/photos/a-city-in-the-mountains-lyoFZoZNx0c",
+  },
+  {
+    name: "Giza, Egypt",
+    link: "https://unsplash.com/photos/pyramid-of-giza-Nsi6jZ9tRZo",
+  },
+  {
+    name: "Chichén Itzá, Mexico",
+    link: "https://unsplash.com/photos/a-very-tall-pyramid-with-a-clock-on-its-side-o3IOpem3bwA",
+  },
+  {
+    name: "Taj Mahal, India",
+    link: "https://unsplash.com/photos/a-white-building-with-a-blue-sky-y023qB37LVc",
+  },
+];

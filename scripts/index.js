6e47474c03f2b7a69011220eec7c83e0500e213f
Sam Branham <hotdogsb@gmail.com>
2024-11-12 00:42:32 -0600

    Add new post submission and card creation

--- scripts/index.js
+++ scripts/index.js
@@ -39,14 +39,19 @@ const profileName = document.querySelector(".profile__name");
 const profileDesc = document.querySelector(".profile__description");
 // edit profile form
 const modalForm = document.forms["profile-form"];
-const modalNameInput = document.querySelector("#input-name");
-const modalDescInput = document.querySelector("#input-description");
+const modalNameInput = modalForm.querySelector("#input-name");
+const modalDescInput = modalForm.querySelector("#input-description");
 
 // add card elements
 const addCardModal = document.querySelector("#add-card-modal");
 const addCardOpenButton = document.querySelector(".profile__add-button");
 const addCardExitButton = addCardModal.querySelector(".modal__exit-button");
+// add card form
+const addCardForm = document.forms["add-card-form"];
+const addCardFormLink = addCardForm.querySelector("#add-card-input-link");
+const addCardFormName = addCardForm.querySelector("#add-card-input-name");
 
+// edit profile events
 profileEditButton.addEventListener("click", () => {
   openModal(editProfileModal);
   modalNameInput.value = profileName.textContent;
@@ -62,11 +67,19 @@ modalForm.addEventListener("submit", (evt) => {
   profileDesc.textContent = modalDescInput.value;
 });
 
+// add card events
 addCardOpenButton.addEventListener("click", () => {
   openModal(addCardModal);
 });
 addCardExitButton.addEventListener("click", () => {
   closeModal(addCardModal);
+});
+addCardForm.addEventListener("submit", (evt) => {
+  evt.preventDefault();
+  closeModal(addCardModal);
+  cardList.prepend(
+    getCardElement({ link: addCardFormLink.value, name: addCardFormName.value })
+  );
 });
 
 function openModal(modal) {
@@ -76,7 +89,7 @@ function closeModal(modal) {
   modal.classList.remove("modal_opened");
 }
 
-function getCardElement(data) {
+function getCardElement(cardData) {
   // Create Card From Template
   let cardElement = cardTemplate.querySelector(".card").cloneNode(true);
 
@@ -85,9 +98,9 @@ function getCardElement(data) {
   let cardImage = cardElement.querySelector(".card__image");
 
   // Assign card data
-  cardTitle.textContent = data.name;
-  cardImage.src = data.link;
-  cardImage.alt = data.name;
+  cardTitle.textContent = cardData.name;
+  cardImage.src = cardData.link;
+  cardImage.alt = cardData.name;
 
   return cardElement;
 }
